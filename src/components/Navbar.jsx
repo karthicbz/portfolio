d32f83c74e88a643660901f24070441efe275d95
Karthick <pkarthicbz@live.in>
2023-07-20 09:13:54 +0530

    added navbar and initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Projects</li>
+        <li>Resume</li>
+        <li>mode</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
