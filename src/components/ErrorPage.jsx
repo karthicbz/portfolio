01b5b5e25069846bb8514cd9236b6138cefe1873
karthicbz <pkarthicbz@live.in>
2023-07-25 19:59:13 +0530

    added text and designed error page

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,7 +1,49 @@
 import React from "react";
+import styled from "styled-components";
 
 const ErrorPage = () => {
-  return <h1>Error Page</h1>;
+  const Div = styled.div`
+    grid-column: 1/2;
+    font-family: "Ubuntu", sans-serif;
+    /* max-width: 700px;
+    width: 700px;
+    margin: 0 auto; */
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 10px;
+
+    grid-row: 2/3;
+    & > .error-code {
+      font-size: 5rem;
+      font-weight: 700;
+      color: fuchsia;
+      filter: blur(2px);
+    }
+
+    & > .error-text {
+      font-size: 1.5rem;
+      color: green;
+    }
+
+    & > .error-img {
+      border-radius: 8px;
+    }
+  `;
+
+  return (
+    <Div>
+      <p className="error-code">404</p>
+      <p className="error-text">
+        The page you are looking is not found. But here's the bear...
+      </p>
+      <img
+        src="https://placebear.com/200/300"
+        alt="a bear"
+        className="error-img"
+      />
+    </Div>
+  );
 };
 
 export default ErrorPage;
